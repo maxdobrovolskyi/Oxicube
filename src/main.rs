56d27fa78ed3617c3288754ac8592b50bb3b4a56
Maksym Dobrovolskyi <mdobrovolskyi@wedlakebell.com>
2026-03-25 11:03:22 +0000

    simple triangle shader + render

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use winit::keyboard::KeyCode;
 //tacing
 use tracing::info;
 //wgpu
-use wgpu;
+use wgpu::{self};
 
 //main
 use std::sync::Arc;
@@ -20,6 +20,7 @@ struct GpuContext {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
 }
 struct App {
     window: Option<Arc<dyn Window>>,
@@ -72,16 +73,62 @@ impl GpuContext {
             .unwrap_or(surface_caps.formats[0]);
 
         let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT, // Рисуем в surface
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT, 
             format: surface_format,
             width: size.width,
             height: size.height,
-            present_mode: wgpu::PresentMode::Fifo, // VSync
+            present_mode: wgpu::PresentMode::Fifo, 
             alpha_mode: surface_caps.alpha_modes[0],
             view_formats: vec![],
             desired_maximum_frame_latency: 2,
         };
         surface.configure(&device, &config);
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Pipeline Layout"),
+            bind_group_layouts: &[],
+            ..Default::default()
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            multiview_mask: Default::default(),
+            label: Some("Render Pipeline"),
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                buffers: &[],   
+                compilation_options: Default::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: surface_format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: Default::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                unclipped_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            cache: None,
+            
+        });
 
         Self {
             surface,
@@ -89,6 +136,7 @@ impl GpuContext {
             queue,
             config,
             size,
+            render_pipeline,
         }
     }
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
@@ -135,18 +183,15 @@ impl ApplicationHandler for App {
                     );
 
                     {
-                        let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                             label: Some("Render Pass"),
                             color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                                 view: &view,
                                 resolve_target: None,
-                                 depth_slice: None,
+                                depth_slice: None,
                                 ops: wgpu::Operations {
                                     load: wgpu::LoadOp::Clear(wgpu::Color {
-                                        r: 0.0,
-                                        g: 0.0,
-                                        b: 1.0,  // ← синий!
-                                        a: 1.0,
+                                        r: 0.1, g: 0.1, b: 0.15, a: 1.0,
                                     }),
                                     store: wgpu::StoreOp::Store,
                                 },
@@ -154,9 +199,15 @@ impl ApplicationHandler for App {
                             depth_stencil_attachment: None,
                             ..Default::default()
                         });
+
+                        render_pass.set_pipeline(&gpu.render_pipeline);  
+                        render_pass.draw(0..3, 0..1);   
                     }
                     gpu.queue.submit(std::iter::once(encoder.finish()));
                     output.present();
+                    if let Some(w) = &self.window {
+                        w.request_redraw();
+                    } 
                 }
 
                 
